e25c1f580159e432af51bf81fbf99cd4d17418d0
dersanli <dersanli@gmail.com>
2017-03-15 16:45:02 +0300

    initial commit

---
+++ Main.py
@@ -0,0 +1,29 @@
+from Measurement import Measurement
+
+values = []
+
+
+def read_measurements():
+    print Measurement.__doc__
+
+    with open(r'd:\WTPLCTAGS.csv', 'r') as f:
+        for line in f:
+            rows = line.split(";")
+            if rows[1] == "READDATETIME":
+                continue
+            m = Measurement()
+            m.windspeed = rows[2]
+            m.winddirection = rows[3]
+            
+            values.append(m)
+
+
+def print_measurements():
+    for k in values:
+        print k.windspeed, k.winddirection
+
+
+if __name__ == "__main__":
+    import sys
+    read_measurements()
+    print_measurements()
---
+++ Measurement.py
@@ -0,0 +1,6 @@
+class Measurement:
+    """Wind Turbine Measurement - PLC Tag Values"""
+
+    def __init__(self):
+        self.windspeed = 0.0
+        self.winddirection = 0
