4b2f2ccbd7c9c5f96460b95577f1e7d077844996
dersanli <dersanli@gmail.com>
2017-03-21 16:38:00 +0300

    started influxdb integration

--- Main.py
+++ Main.py
@@ -1,7 +1,44 @@
-import sys
 from Measurement import Measurement
+import requests
+import datetime
 
 values = []
+
+# WTPLCTAGID                   int            0
+# READDATETIME                 datetime       1
+# WINDSPEED                    float          2
+# WINDDIRECTION                int            3
+# TURBINEDIRECTION             int            4
+# LOWSPEEDRPM                  float          5
+# HIGHSPEEDRPM                 float          6
+# BRAKEOPEN                    bool?          7
+# EMERGENCYBUTTON              bool?          8
+# MANUALMODE                   bool?          9
+# STATESECTION                 bool?         10
+# HUBBATTERYVOLTAGE            float         11
+# EXTERNALTEMP                 float         12
+# INTERNALTEMP                 float         13
+# GEARBOXTEMP                  float         14
+# HUBTEMP                      float         15
+# BEARINGTEMP                  float         16
+# GEARBOXOILLEVELLOW           bool?         17
+# YAWDRIVEERROR                bool?         18
+# ETHERNETCOMMS                bool?         19
+# ALTSWITCHON                  bool?         20
+# ALTSWITCHOFF                 bool?         21
+# HIGHSPEEDRPMSETVALUE         int           22
+# BLADEPITCHMAXSETVALUE        int           23
+# PACALTERNATORVOLTAGEL1       float         24
+# PACALTERNATORVOLTAGEL2       float         25
+# PACALTERNATORVOLTAGEL3       float         26
+# PACALTERNATORCURRENTL1       float         27
+# PACALTERNATORCURRENTL2       float         28
+# PACALTERNATORCURRENTL3       float         29
+# PACALTERNATORFREQUENCY       int           30
+
+
+querystring = 'windmeasurement, brakeopen=@BO@, emergencybutton=@EB@ windspeed=@WS@, winddirection=@WD@, ' \
+              'turbinedirection=@TD@, highspeedrpm=@HSR@ @@DT@@'
 
 
 def read_measurements():
@@ -13,17 +50,28 @@ def read_measurements():
             if rows[1] == "READDATETIME":
                 continue
             m = Measurement()
+            m.readdatetime = datetime.datetime.strptime(rows[1], "%Y-%m-%d %H:%M:%S.%f").isoformat() + 'Z'
             m.windspeed = rows[2]
             m.winddirection = rows[3]
+            m.turbinedirection = rows[4]
+            m.lowspeedrpm = rows[5]
+            m.highspeedrpm = rows[6]
+            m.brakeopen = rows[7]
+            m.emergencybutton = rows[8]
 
             values.append(m)
 
 
 def print_measurements():
     for k in values:
-        print k.windspeed, k.winddirection
+        print k.readdatetime, k.windspeed, k.winddirection, k.turbinedirection, k.lowspeedrpm, k.highspeedrpm
 
 
 if __name__ == "__main__":
     read_measurements()
     print_measurements()
+    payload = 'windspeed,region=tr-center value=' + repr(windspeed)
+    try:
+        r = requests.post('http://dev.soyut.com:8086/write?db=mydb2', data=payload)
+    except requests.exceptions.RequestException as e:
+        print "Connection Error. Retrying - ", e
--- Measurement.py
+++ Measurement.py
@@ -2,5 +2,33 @@ class Measurement:
     """Wind Turbine Measurement - PLC Tag Values"""
 
     def __init__(self):
+        self.readdatetime = None
         self.windspeed = 0.0
         self.winddirection = 0
+        self.turbinedirection = 0
+        self.lowspeedrpm = 0.0
+        self.highspeedrpm = 0.0
+        self.brakeopen = 0
+        self.emergencybutton = 0
+        self.manualmode = 0
+        self.statesection = 0
+        self.hubbatteryvoltage = 0.0
+        self.externaltemp = 0.0
+        self.internaltemp = 0.0
+        self.gearboxtemp = 0.0
+        self.hubtemp = 0.0
+        self.bearingtemp = 0.0
+        self.gearboxoillevellow = 0
+        self.yawdriveerror = 0
+        self.ethernetcomms = 0
+        self.altswitchon = 0
+        self.altswitchoff = 0
+        self.highspeedrpmsetvalue = 0
+        self.bladepitchmaxsetvalue = 0
+        self.pacalternatorvoltageL1 = 0.0
+        self.pacalternatorvoltageL2 = 0.0
+        self.pacalternatorvoltageL3 = 0.0
+        self.pacalternatorcurrentL1 = 0.0
+        self.pacalternatorcurrentL2 = 0.0
+        self.pacalternatorcurrentL3 = 0.0
+        self.pacalternatorfrequency = 0.0
